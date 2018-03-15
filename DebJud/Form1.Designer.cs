e8338073cfe896f72ca7fa1a95df8d13654ad9eb
Denis Faria <denis.tavares@divicred.com.br>
2018-03-15 16:27:33 -0300

    Inicial

---
+++ DebJud/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace DebJud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(475, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(88, 130);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(475, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(569, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(36, 21);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(569, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(36, 21);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(692, 45);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(69, 105);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Processar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 213);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(558, 150);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.Visible = false;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(650, 213);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(524, 150);
+            this.dataGridView2.TabIndex = 6;
+            this.dataGridView2.Visible = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(190, 182);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(49, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "IPVA";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(374, 182);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(64, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Protesto";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Saldo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Dados";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 229);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DebJud/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.IO;
+using System.Data.OleDb;
+//using Excel = Microsoft.Office.Interop.Excel;
+using ClosedXML.Excel;
+using System.Globalization;
+using Spire.Xls;
+
+namespace DebJud
+{
+    public partial class Form1 : Form
+    {
+        private string caminhoPlanilhaSaldo;
+        private string caminhoPlanilhaTarifa;
+        private DataSet mDataSet;
+        private MySqlConnection mConn;
+        bool existeDiretorio;
+        int inicioPlanilhaSaldo;
+        int inicioPlanilhaTarifa;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                caminhoPlanilhaSaldo = textBox1.Text;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = openFileDialog2.FileName;
+                caminhoPlanilhaTarifa = textBox2.Text;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //    try
+            //    {
+            //Conexão com banco
+            mDataSet = new DataSet();
+            mConn = new MySqlConnection("Server=10.11.17.30;Database=4030;Uid=root;Pwd=chinchila@acida12244819;");
+            //mConn = new MySqlConnection("Server=192.168.0.107;Database=tarifa;Uid=Denis;Pwd=Dtf@4030;");
+            mConn.Open();
+
+            if (caminhoPlanilhaSaldo == null || caminhoPlanilhaTarifa == null)
+                MessageBox.Show("Selecione os caminhos dos arquivos");
+
+            if (existeDiretorio == false)
+                System.IO.Directory.CreateDirectory("c:/RCArquivos");
+
+            if (existeDiretorio == false)
+                System.IO.Directory.CreateDirectory("c:/RCArquivos/auxiliar");
+
+
+            string descricao = "";
+            string codigoMov = "";
+
+            if (radioButton1.Checked)
+            {
+                descricao = "REF. AO PGTO DE IPVA, PROTOCOLO";
+                codigoMov = "61";
+            }
+            else
+            {
+                descricao = "REF. AO PGTO DE PROTESTO, PROTOCOLO";
+                codigoMov = "60";
+            }
+
+            StreamWriter writePrincipal = new StreamWriter(@"C:\RCArquivos\Tarifas-" + DateTime.Now.Date.ToString("yyyyMMdd") + ".txt");
+            writePrincipal.WriteLine("0175640300001562SICOOBDIVI" + codigoMov + "03" + DateTime.Now.Date.ToString("yyyyMMdd") + "                                                                                                                                                                  ");
+
+
+            Conexao conectaPlanilha = new Conexao();
+            DataSet output = conectaPlanilha.importarExcel(caminhoPlanilhaSaldo, caminhoPlanilhaTarifa);
+
+            var wb = new XLWorkbook();//Varíavel para a planilha(Pasta de trabalho)
+            var ws = wb.Worksheets.Add("DADOS");//Variavel para a planilha dentro do workbook
+
+            ws.Cell("A1").Value = "Conta";
+            ws.Cell("B1").Value = "Valor";
+            ws.Cell("C1").Value = "Protocolo";
+
+            dataGridView1.DataSource = output.Tables[0];
+            dataGridView2.DataSource = output.Tables[1];
+
+            for (int j = 0; j <= output.Tables[0].Rows.Count; j++)
+            {
+                if (output.Tables[0].Rows[j]["F1"].ToString().Length > 0)
+                {
+                    if (!output.Tables[0].Rows[j]["F1"].ToString().Contains("Conta"))
+                    {
+                        inicioPlanilhaSaldo = j;
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i <= output.Tables[1].Rows.Count; i++)
+            {
+                string nomeColuna1 = dataGridView2.Columns[0].HeaderText;
+
+                if (output.Tables[1].Rows[i][nomeColuna1].ToString().Length > 0)
+                {
+                    if (!output.Tables[1].Rows[i][nomeColuna1].ToString().Contains("Conta"))
+                    {
+                        inicioPlanilhaTarifa = i;
+                        break;
+                    }
+                }
+            }
+
+
+            string nomeColunaContaArquivo = dataGridView2.Columns[0].HeaderText;
+            string nomeColunaValorArquivo = dataGridView2.Columns[1].HeaderText;
+            string nomeColunaProtocoloArquivo = dataGridView2.Columns[2].HeaderText;
+
+            string nomeColunaContaSaldo = dataGridView1.Columns[0].HeaderText;
+            string nomeColunaValorSaldoDisponivel = dataGridView1.Columns[25].HeaderText;
+            string nomeColunaValorLimiteDisponivel = dataGridView1.Columns[34].HeaderText;
+
+            int linha = 2;
+
+            for (int i = inicioPlanilhaTarifa; i < output.Tables[1].Rows.Count; i++)
+            {
+                string contaArquivo = output.Tables[1].Rows[i][nomeColunaContaArquivo].ToString();
+                double valorArquivo = Convert.ToDouble(output.Tables[1].Rows[i][nomeColunaValorArquivo]);
+                int ProtocoloArquivo = Convert.ToInt32(output.Tables[1].Rows[i][nomeColunaProtocoloArquivo]);
+
+                for (int j = inicioPlanilhaSaldo; j < output.Tables[0].Rows.Count; j++)
+                {
+                    string contaSaldo = output.Tables[0].Rows[j][nomeColunaContaSaldo].ToString();
+
+
+
+                    if (contaArquivo == contaSaldo)
+                    {
+                        
+                        double valorSaldoDisponivel = Convert.ToDouble(output.Tables[0].Rows[j][nomeColunaValorSaldoDisponivel]);
+                        double valorLimiteDisponivel = Convert.ToDouble(output.Tables[0].Rows[j][nomeColunaValorLimiteDisponivel]);
+                        double saldo = Math.Round(valorLimiteDisponivel + valorSaldoDisponivel, 2);
+
+                        if (saldo >= valorArquivo)
+                        {
+                            writePrincipal.WriteLine("1D" + contaSaldo.Replace("-", "").Replace(".", "").PadLeft(10, '0') + "                                                                  000                              " + valorArquivo.ToString("N2").Replace(".", "").Replace(",", "").PadLeft(17, '0') +
+                                                                    "          000N" + descricao.PadLeft(40, ' ').Replace("Ç", "C").Replace("Á", "A").Replace("É", "E").Replace("Ã", "A").Replace("Õ", "O").Replace("Í", "I").Replace("Ó", "O").Replace("Ê", "E").Replace("Ô", "O") + "                  ");
+
+                        }
+                        else if (saldo > 0)
+                        {
+                            double novoValor = valorArquivo - saldo;
+                            ws.Cell("A" + linha.ToString()).Value = novoValor;
+
+                            writePrincipal.WriteLine("1D" + contaSaldo.Replace("-", "").Replace(".", "").PadLeft(10, '0') + "                                                                  000                              " + saldo.ToString("N2").Replace(".", "").Replace(",", "").PadLeft(17, '0') +
+                                                                   "          000N" + descricao.PadLeft(40, ' ').Replace("Ç", "C").Replace("Á", "A").Replace("É", "E").Replace("Ã", "A").Replace("Õ", "O").Replace("Í", "I").Replace("Ó", "O").Replace("Ê", "E").Replace("Ô", "O") + "                  ");
+
+                            ws.Cell("A" + linha.ToString()).Value = contaSaldo;
+                            ws.Cell("B" + linha.ToString()).Value = novoValor ;
+                            ws.Cell("C" + linha.ToString()).Value = ProtocoloArquivo;
+                            linha++;
+                        }
+                        else if (saldo <= 0)
+                        {
+                            ws.Cell("A" + linha.ToString()).Value = contaSaldo;
+                            ws.Cell("B" + linha.ToString()).Value = valorArquivo;
+                            ws.Cell("C" + linha.ToString()).Value = ProtocoloArquivo;
+                            linha++;
+                        }
+
+                    }
+                }
+
+
+
+                //Liberar objetos da memoria
+                ws.Dispose();
+                wb.Dispose();
+
+                //Salvar o arquivo no disco
+                wb.SaveAs(@"c:\RCArquivos\auxiliar\" + DateTime.Now.Hour.ToString() + "-" + DateTime.Now.Minute.ToString() + ".xlsx");
+
+                Workbook teste = new Workbook();
+                teste.LoadFromFile(@"c:\RCArquivos\auxiliar\" + DateTime.Now.Hour.ToString() + "-" + DateTime.Now.Minute.ToString() + ".xlsx");
+                teste.SaveToFile(@"C:\RCArquivos\Convertido"+ DateTime.Now.Minute.ToString() + ".xls", ExcelVersion.Version97to2003);
+
+
+            }
+
+
+            writePrincipal.Dispose();
+            writePrincipal.Close();
+            //}
+            // catch (Exception exception)
+            // {
+            //    Console.WriteLine(exception);
+            //     throw;
+            //   }
+            MessageBox.Show("Arquivo Gerado com sucesso!!");
+        }
+
+
+
+    }
+}
